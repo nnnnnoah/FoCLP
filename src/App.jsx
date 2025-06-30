81b709d892001dc7499cc809e218f0df98823e12
noah <noahmiller@hotmail.ca>
2025-06-30 16:43:52 -0400

    Initial commit (general component structure)

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+import React from "react";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import PageContent from "./components/PageContent";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <main>
+        <PageContent />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="">
+      <a href="/">
+        <img className="logo" src="/assets/images/logo.png"></img>
+      </a>
+    </header>
+  );
+}
+
+export default Header;
