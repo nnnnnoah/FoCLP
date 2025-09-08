729a7e2f92ddd654ddd7c2a8766ecc072c30e39c
noah <noahmiller@hotmail.ca>
2025-09-08 17:32:02 -0400

    feat: production vs dev routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -8,7 +8,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router basename={"/FoCLP"}>
+    <Router>
       <Header />
       <Navbar />
       <Routes>
